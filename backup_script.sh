63e05dcb1437541bd853070434f020ad5d4931af
bvtri98 <bvtri98@gmail.com>
2022-04-16 16:06:45 +0700

    Regular auto-commit 16042022_160645

--- backup_script.sh
+++ backup_script.sh
@@ -5,15 +5,12 @@ timestamp() {
   date +"%d%m%Y_%H%M%S"
 }
 
-# create folder include all file
-mkdir wp_$(timestamp)
-
 # compress source code wordpress
 echo "########################## Zipping file #########################"
-zip -r wp_$(timestamp)/wordpress.zip /var/www/html/wordpress
+zip -r backup/wordpress_$(timestamp).zip /var/www/html/wordpress
 #export sql database
 echo "########################## Export sql file #########################"
-mysqldump -uroot -p'1890' wordpress_db > wp_$(timestamp)/wordpress_db.sql
+mysqldump -uroot -p'1890' wordpress_db > backup/wordpress_db_$(timestamp).sql
 
 
 # git flow
